8e7118701568ab5dbda3d6147af0202deaedcc13
Juan Manuel Reyes Garcia <seyerman@gmail.com>
2020-08-23 20:09:31 -0500

    Agregar archivos de proyecto.

---
+++ exploring-regionsandcities-wf/RegionAndCitiesForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace exploring_regionsandcities_wf
+{
+    partial class RegionAndCitiesForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.butOpenFile = new System.Windows.Forms.Button();
+            this.regionAndCityDataView = new System.Windows.Forms.DataGridView();
+            this.region = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deptCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.department = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cityCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.city = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labRegion = new System.Windows.Forms.Label();
+            this.cbRegions = new System.Windows.Forms.ComboBox();
+            this.butClearTable = new System.Windows.Forms.Button();
+            this.graphicReport = new System.Windows.Forms.Button();
+            this.barCharRegionsDepts = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.regionAndCityDataView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barCharRegionsDepts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.InitialDirectory = "./";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(12, 23);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(474, 20);
+            this.txtFileName.TabIndex = 0;
+            // 
+            // butOpenFile
+            // 
+            this.butOpenFile.Location = new System.Drawing.Point(491, 21);
+            this.butOpenFile.Name = "butOpenFile";
+            this.butOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.butOpenFile.TabIndex = 1;
+            this.butOpenFile.Text = "Open File";
+            this.butOpenFile.UseVisualStyleBackColor = true;
+            this.butOpenFile.Click += new System.EventHandler(this.openFileClick);
+            // 
+            // regionAndCityDataView
+            // 
+            this.regionAndCityDataView.AllowUserToAddRows = false;
+            this.regionAndCityDataView.AllowUserToDeleteRows = false;
+            this.regionAndCityDataView.AllowUserToOrderColumns = true;
+            this.regionAndCityDataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.regionAndCityDataView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.region,
+            this.deptCode,
+            this.department,
+            this.cityCode,
+            this.city});
+            this.regionAndCityDataView.Location = new System.Drawing.Point(12, 90);
+            this.regionAndCityDataView.Name = "regionAndCityDataView";
+            this.regionAndCityDataView.ReadOnly = true;
+            this.regionAndCityDataView.Size = new System.Drawing.Size(554, 321);
+            this.regionAndCityDataView.TabIndex = 2;
+            // 
+            // region
+            // 
+            this.region.HeaderText = "REGION";
+            this.region.Name = "region";
+            this.region.ReadOnly = true;
+            // 
+            // deptCode
+            // 
+            this.deptCode.HeaderText = "DEPT CODE";
+            this.deptCode.Name = "deptCode";
+            this.deptCode.ReadOnly = true;
+            // 
+            // department
+            // 
+            this.department.HeaderText = "DEPARTMENT";
+            this.department.Name = "department";
+            this.department.ReadOnly = true;
+            // 
+            // cityCode
+            // 
+            this.cityCode.HeaderText = "CITY CODE";
+            this.cityCode.Name = "cityCode";
+            this.cityCode.ReadOnly = true;
+            // 
+            // city
+            // 
+            this.city.HeaderText = "CITY";
+            this.city.Name = "city";
+            this.city.ReadOnly = true;
+            // 
+            // labRegion
+            // 
+            this.labRegion.AutoSize = true;
+            this.labRegion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labRegion.Location = new System.Drawing.Point(14, 56);
+            this.labRegion.Name = "labRegion";
+            this.labRegion.Size = new System.Drawing.Size(71, 20);
+            this.labRegion.TabIndex = 3;
+            this.labRegion.Text = "Region:";
+            // 
+            // cbRegions
+            // 
+            this.cbRegions.FormattingEnabled = true;
+            this.cbRegions.Location = new System.Drawing.Point(101, 55);
+            this.cbRegions.Name = "cbRegions";
+            this.cbRegions.Size = new System.Drawing.Size(178, 21);
+            this.cbRegions.TabIndex = 4;
+            this.cbRegions.SelectedIndexChanged += new System.EventHandler(this.cbRegionsSelectedIndexChanged);
+            // 
+            // butClearTable
+            // 
+            this.butClearTable.Location = new System.Drawing.Point(491, 53);
+            this.butClearTable.Name = "butClearTable";
+            this.butClearTable.Size = new System.Drawing.Size(75, 23);
+            this.butClearTable.TabIndex = 5;
+            this.butClearTable.Text = "Clear Table";
+            this.butClearTable.UseVisualStyleBackColor = true;
+            this.butClearTable.Click += new System.EventHandler(this.clearTableClick);
+            // 
+            // graphicReport
+            // 
+            this.graphicReport.Location = new System.Drawing.Point(371, 53);
+            this.graphicReport.Name = "graphicReport";
+            this.graphicReport.Size = new System.Drawing.Size(101, 23);
+            this.graphicReport.TabIndex = 6;
+            this.graphicReport.Text = "Graphic Report";
+            this.graphicReport.UseVisualStyleBackColor = true;
+            this.graphicReport.Click += new System.EventHandler(this.graphicReportClick);
+            // 
+            // barCharRegionsDepts
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.barCharRegionsDepts.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.barCharRegionsDepts.Legends.Add(legend3);
+            this.barCharRegionsDepts.Location = new System.Drawing.Point(586, 23);
+            this.barCharRegionsDepts.Name = "barCharRegionsDepts";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.barCharRegionsDepts.Series.Add(series3);
+            this.barCharRegionsDepts.Size = new System.Drawing.Size(518, 388);
+            this.barCharRegionsDepts.TabIndex = 7;
+            this.barCharRegionsDepts.Text = "barCharRegionsDept";
+            // 
+            // RegionAndCitiesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 424);
+            this.Controls.Add(this.barCharRegionsDepts);
+            this.Controls.Add(this.graphicReport);
+            this.Controls.Add(this.butClearTable);
+            this.Controls.Add(this.cbRegions);
+            this.Controls.Add(this.labRegion);
+            this.Controls.Add(this.regionAndCityDataView);
+            this.Controls.Add(this.butOpenFile);
+            this.Controls.Add(this.txtFileName);
+            this.Name = "RegionAndCitiesForm";
+            this.Text = "Regions & Cities";
+            ((System.ComponentModel.ISupportInitialize)(this.regionAndCityDataView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barCharRegionsDepts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button butOpenFile;
+        private System.Windows.Forms.DataGridView regionAndCityDataView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn region;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deptCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn department;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cityCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn city;
+        private System.Windows.Forms.Label labRegion;
+        private System.Windows.Forms.ComboBox cbRegions;
+        private System.Windows.Forms.Button butClearTable;
+        private System.Windows.Forms.Button graphicReport;
+        private System.Windows.Forms.DataVisualization.Charting.Chart barCharRegionsDepts;
+    }
+}
+
---
+++ exploring-regionsandcities-wf/RegionAndCitiesForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Security.Policy;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace exploring_regionsandcities_wf
+{
+    public partial class RegionAndCitiesForm : Form
+    {
+        private const string REGION = "REGION";
+        private List<string[]> values;
+
+        public RegionAndCitiesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileClick(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            clearTable();
+            Console.WriteLine(openFileDialog.FileName);
+            values = new List<string[]>();
+
+            txtFileName.Text = openFileDialog.FileName;
+            string[] lines = System.IO.File.ReadAllLines(openFileDialog.FileName);
+
+            Dictionary<string, bool> regions = new Dictionary<string,bool>();
+            foreach (string line in lines){
+                string[] rowLine = line.Split(',');
+
+                if (!rowLine[0].Equals(REGION)) {
+                    values.Add(rowLine);
+                    regionAndCityDataView.Rows.Add(rowLine);
+                }
+
+                if (!regions.ContainsKey(rowLine[0])) {
+                    regions.Add(rowLine[0],true);
+                    cbRegions.Items.Add(rowLine[0]);
+                }
+            }
+        }
+
+        private void cbRegionsSelectedIndexChanged(object sender, EventArgs e)
+        {
+            clearTable();
+            foreach (string[] row in values){
+                if (cbRegions.SelectedItem.Equals(row[0]) || cbRegions.SelectedItem.Equals(REGION)) {
+                    regionAndCityDataView.Rows.Add(row);
+                }
+            }
+        }
+
+        private void clearTableClick(object sender, EventArgs e)
+        {
+            clearTable();
+        }
+
+        private void clearTable()
+        {
+            regionAndCityDataView.Rows.Clear();
+        }
+
+        private void graphicReportClick(object sender, EventArgs e)
+        {
+            string serieName = "Departments by Region";
+            barCharRegionsDepts.Series.Clear();
+            barCharRegionsDepts.Series.Add(serieName);
+
+            Dictionary<string,Dictionary<string,int>> regions = new Dictionary<string,Dictionary<string,int>>();
+            foreach (string[] row in values) {
+                string reg = row[0];
+                string dept = row[2];
+                if (!regions.ContainsKey(reg)) {
+                    regions.Add(reg,new Dictionary<string, int>());
+                    regions[reg].Add(dept,1);
+                }
+                else {
+                    if (!regions[reg].ContainsKey(dept)) {
+                        regions[reg].Add(dept,1);
+                    }
+                    else {
+                        regions[reg][dept] += 1;
+                    }
+                }
+            }
+
+            foreach(KeyValuePair<string, Dictionary<string,int>> kvp in regions) {
+                barCharRegionsDepts.Series[serieName].Points.AddXY(kvp.Key,kvp.Value.Count);
+            }
+
+            barCharRegionsDepts.Series[serieName].ChartType = SeriesChartType.Bar;
+        }
+    }
+}
